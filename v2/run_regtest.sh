ad9bca7ac5b82e47fb2416b8354cde3b303e7fb3
liaogang <liaogang@baidu.com>
2017-04-21 16:45:07 +0800

    add regression test v2

---
+++ v2/run_regtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+#paddle book image name
+if [ ! -n "$1" ]; then
+  book_image=paddlepaddle/book
+else
+  book_image=$1
+fi
+
+#paddle book image tag
+if [ ! -n "$2" ]; then
+  book_tag=latest
+else
+  book_tag=$2
+fi
+
+#regression test
+docker run -it --rm -p 8888:8888 -v "$PWD:/reg_test" ${book_image}:${book_tag} /bin/bash /reg_test/demo.sh
