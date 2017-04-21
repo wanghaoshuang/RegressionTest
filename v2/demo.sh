7890d2190a5a705bb106acd2a51f6342f3e2540f
gangliao <liaogang@baidu.com>
2017-04-21 18:41:58 +0800

    fix fit line name

--- v2/demo.sh
+++ v2/demo.sh
@@ -6,8 +6,8 @@ export DISABLE_PLOT=True
 pip install /usr/opt/paddle/share/wheels/*.whl
 
 #01.fit_a_line
-jupyter nbconvert --to python book/01.fit_a_lin/README.ipynb --stdout | python
-jupyter nbconvert --to python book/01.fit_a_lin/README.en.ipynb --stdout | python
+jupyter nbconvert --to python book/01.fit_a_line/README.ipynb --stdout | python
+jupyter nbconvert --to python book/01.fit_a_line/README.en.ipynb --stdout | python
 
 #02.recognize_digits
 jupyter nbconvert --to python book/02.recognize_digits/README.ipynb --stdout | python
