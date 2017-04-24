7b15cffc124ec91588c2f98c78e9c6b215676078
liaogang <liaogang@baidu.com>
2017-04-24 16:33:18 +0800

    add option for reg test

--- v2/demo.sh
+++ v2/demo.sh
@@ -14,13 +14,19 @@ pip install pillow
 
 function reg_test() {
   for i in $@; do
-    notedown $i > ${i%.*}.ipynb
+    notedown --match=python $i > ${i%.*}.ipynb
     jupyter nbconvert --to python ${i%.*}.ipynb --stdout | python
   done
 }
 
-#01.fit_a_line
-reg_test /book/01.fit_a_line/README.md /book/01.fit_a_line/README.en.md
-
-#02.recognize_digits
-reg_test /book/02.recognize_digits/README.md /book/02.recognize_digits/README.en.md
+if [ "$1" ]; then
+  if [ "1" = "$1" ]; then
+    #01.fit_a_line
+    reg_test /book/01.fit_a_line/README.md /book/01.fit_a_line/README.en.md
+  elif [ "2" = "$1" ]; then
+    #02.recognize_digits
+    reg_test /book/02.recognize_digits/README.md /book/02.recognize_digits/README.en.md
+  else
+    echo "Not implemented!"
+  fi
+fi
--- v2/run_regtest.sh
+++ v2/run_regtest.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set -e
+set -xe
 
 cur_path="$(cd "$(dirname "$0")" && pwd -P)"
 
 #paddle book image name
-if [ ! -n "$1" ]; then
+if [ ! -n "$2" ]; then
   book_image=paddlepaddle/book
 else
-  book_image=$1
+  book_image=$2
 fi
 
 #paddle book image tag
-if [ ! -n "$2" ]; then
+if [ ! -n "$3" ]; then
   book_tag=latest
 else
-  book_tag=$2
+  book_tag=$3
 fi
 
 #regression test
-docker run -i --rm -p 8888:8888 -v "${cur_path}:/reg_test" ${book_image}:${book_tag} /bin/bash /reg_test/demo.sh
+docker run -i --rm -p 8888:8888 -v "${cur_path}:/reg_test" ${book_image}:${book_tag} /bin/bash -c "/bin/bash ./reg_test/demo.sh $1"
