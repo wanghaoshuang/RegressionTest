be4a251912e06d4a5c77d84284f15d11fe02ed57
liaogang <liaogang@baidu.com>
2017-04-24 10:59:47 +0800

    add test patch

--- v2/demo.sh
+++ v2/demo.sh
@@ -3,7 +3,12 @@ set -e
 
 export DISABLE_PLOT=True
 
+apt-get install patch
 find /usr/ -name '*paddle-*whl' | xargs pip install
+
+cd /book && patch -p0 </reg_test/test.patch
+
+bash .tools/convert-markdown-into-ipynb-and-test.sh
 
 #01.fit_a_line
 jupyter nbconvert --to python book/01.fit_a_line/README.ipynb --stdout | python
