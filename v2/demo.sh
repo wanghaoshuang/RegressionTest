ea9a46cde32e71733a4d3434a0a0db4957ed6777
liaogang <liaogang@baidu.com>
2017-04-24 11:11:32 +0800

    add go

--- v2/demo.sh
+++ v2/demo.sh
@@ -8,6 +8,12 @@ find /usr/ -name '*paddle-*whl' | xargs pip install
 
 cd /book && patch -p0 </reg_test/test.patch
 
+wget -c http://golangtc.com/static/go/1.8/go1.8.linux-amd64.tar.gz  | tar -C /usr/lib/ -xz && \
+    mkdir -p /usr/share/go
+export GOROOT=/usr/lib/go
+export GOPATH=/usr/share/go
+export PATH=${GOROOT}/bin:${GOPATH}/bin:$PATH
+
 bash .tools/convert-markdown-into-ipynb-and-test.sh
 
 #01.fit_a_line
