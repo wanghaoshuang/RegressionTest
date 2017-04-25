2f774a89890399a150ce87dce4d4fafba6685d07
liaogang <liaogang@baidu.com>
2017-04-25 22:31:53 +0800

    update demo

--- v2/demo.sh
+++ v2/demo.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 set -xe
 
+#disable python plot figures
 export DISABLE_PLOT=True
 
+#patch book v2 demos
 cd /book
-
-
-array=()
-while IFS=  read -r -d $'\0'; do
-    array+=("$REPLY")
-done < <(find /reg_test -name test.patch -print0)
-
-for file in "${array[@]}"; do
-    patch -p0<file
-done
-
-
-/bin/bash /book/.tools/convert-markdown-into-ipynb-and-test.sh
+# find /reg_test -name test.patch | xargs patch -p0
+patch -p0 < /reg_test/02.recognize_digits/test.patch
+#convert markdown to ipynb
+bash /book/.tools/convert-markdown-into-ipynb-and-test.sh
 
 function reg_test() {
   for i in $@; do
@@ -27,13 +20,13 @@ function reg_test() {
 if [ "$1" ]; then
   if [ "1" = "$1" ]; then
     #01.fit_a_line
-    reg_test /book/01.fit_a_line/README.ipynb /book/01.fit_a_line/README.en.ipynb
+    find 01* -name "README*ipynb" | xargs reg_test
   elif [ "2" = "$1" ]; then
     #02.recognize_digits
-    reg_test /book/02.recognize_digits/README.ipynb /book/02.recognize_digits/README.en.ipynb
+    find 02* -name "README*ipynb" | xargs reg_test
   elif [ "3" = "$1" ]; then
     #03.image_classification
-    reg_test /book/03.image_classification/README.ipynb /book/03.image_classification/README.en.ipynb
+    find 03* -name "README*ipynb" | xargs reg_test
   else
     echo "Not implemented!"
   fi
