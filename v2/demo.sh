45a3e76a00619abe48ba86df1f40374060e8f807
liaogang <liaogang@baidu.com>
2017-04-25 23:18:49 +0800

    update reg v2

--- v2/demo.sh
+++ v2/demo.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 set -xe
 
-#disable python plot figures
-export DISABLE_PLOT=True
-
-#patch book v2 demos
 cd /book
-# find /reg_test -name test.patch | xargs patch -p0
-patch -p0 < /reg_test/02.recognize_digits/test.patch
-#convert markdown to ipynb
-bash /book/.tools/convert-markdown-into-ipynb-and-test.sh
 
 function reg_test() {
   for i in $@; do
-    jupyter nbconvert --to python i --stdout | python
+    # patch demo No.i
+    demo_dir=$(dirname $i)
+    # find "$(dirname $i)" -name test.patch | xargs patch -p0
+    patch -p0 < /reg_test/$demo_dir*/test.patch
+    # convert markdown to ipynb
+    bash /book/.tools/convert-markdown-into-ipynb-and-test.sh
+    # execute ipython notebook
+    jupyter nbconvert --to python ${i%.*}.ipynb --stdout | python
   done
 }
 
 if [ "$1" ]; then
-  if [ "1" = "$1" ]; then
-    #01.fit_a_line
-    find 01* -name "README*ipynb" | xargs reg_test
-  elif [ "2" = "$1" ]; then
-    #02.recognize_digits
-    find 02* -name "README*ipynb" | xargs reg_test
-  elif [ "3" = "$1" ]; then
-    #03.image_classification
-    find 03* -name "README*ipynb" | xargs reg_test
-  else
-    echo "Not implemented!"
+  mds=$(find 0$1* -name "README*md" | xargs)
+  if [ "$mds" ]; then
+    reg_test $mds
   fi
 fi
